9e369ce4f87c01ac40458915616baa0a42cbc9ec
Israel Lima <israel_lima_16@hotmail.com>
2020-12-22 15:20:25 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/sec/apicom/devlima/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sec.apicom.devlima.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
