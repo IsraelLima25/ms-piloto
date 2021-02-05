68a0606eac77473265276823718281c139016872
israel <israel_lima_16@hotmail.com>
2021-02-05 09:17:34 -0300

    Db config server

--- hr-api-gateway-zuul/src/main/java/com/devlima/apigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/devlima/apigatewayzuul/config/ResourceServerConfig.java
@@ -29,7 +29,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	private static final String[] PUBLIC = {"/hr-oauth/oauth/token", "/hr-worker/**", "/hr-tester/**"};
 	private static final String[] OPERATOR = {"/hr-worker/**"};
 	private static final String[] ADMIN = {"/hr-payroll/**", "/hr-user/**",
-			"/actuator/**", "/hr-worker/actuator/**"};
+			"/actuator/**", "/hr-worker/actuator/**", "/hr-oauth/actuator/**"};
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
